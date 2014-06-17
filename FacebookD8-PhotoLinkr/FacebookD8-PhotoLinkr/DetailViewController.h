01198f971dfde0e1dcab467285dfea0bdd17d119
Ivan Andriollo <ivanandriollo@gmail.com>
2014-06-17 20:18:12 +0100

    initial commit

---
+++ FacebookD8-PhotoLinkr/FacebookD8-PhotoLinkr/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  FacebookD8-PhotoLinkr
+//
+//  Created by Ivan Andriollo on 17/06/2014.
+//  Copyright (c) 2014 PL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
---
+++ FacebookD8-PhotoLinkr/FacebookD8-PhotoLinkr/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  FacebookD8-PhotoLinkr
+//
+//  Created by Ivan Andriollo on 17/06/2014.
+//  Copyright (c) 2014 PL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
